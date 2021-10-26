ec802fa694acfbc36a063960218ff691562a73d6
Christiane Gross <chgross.wickede@gmail.com>
2021-10-26 16:50:52 +0200

    rewrote deltas for use with only phase instead of complex numbers, results for plaquette are the same for both methods. measured average plaquette value for 8^3x8, delta tau=1 lattices for several beta, behave monotonously

--- plot.gp
+++ plot.gp
@@ -1,13 +1,31 @@
 reset
 set ter pdfcairo size 10 cm, 10 cm
 
-set style line 1 lc 8 pt 7 
+set style line 1 lc 7 pt 7 
 set style line 2 lc 2 pt 6 
+
+file='measbeta1.0Nt16Ns16cnum.txt'
+file2='measbeta1.0Nt16Ns16.txt'
+
+set out 'averageplaquette.pdf'
+
+set title 'Delta tau=1, N_s=N_t=8'
+set ylabel '<P>'
+set xlabel 'beta'
+plot 'meascombined.txt' u 1:5 ls 1 title ''
 
 set out "meas.pdf"
 
 set yrange [0:1]
 
-plot 'meas.txt' u 0:1 ls 1 w lines title 'plaquette', 'meas.txt' u 0:2 ls 2 w lines title 'acceptance rate'
+set xlabel 'configuration'
+set ylabel ''
+
+plot file u 0:1 ls 1 w lines title 'plaquette', file u 0:2 ls 2 w lines title 'acceptance rate'
+plot file2 u 0:1 ls 1 w lines title 'plaquette', file2 u 0:2 ls 2 w lines title 'acceptance rate'
+
+
+set yrange [0.645:0.65]
+plot file u 0:1 ls 2 ps 0.3 title '', file2 u 0:1 ls 1 ps 0.1 title ''
 
 set out
--- u1.c
+++ u1.c
@@ -15,15 +15,24 @@
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
+#include <gsl/gsl_vector.h>
+
+double mean(gsl_vector *vector, int length){
+    double mean=0;
+    for (int i=0; i<length; i+=1){
+        mean+=gsl_vector_get(vector, i);
+    }
+    return mean/length;
+}
 
 /**
  * calculates plaquette as given in paper, uses a* b* =(ab)* for complex numbers
  * **/
-double plaquette(gsl_complex * lattice, int * neighbour, int position, int mu, int nu){
-    gsl_complex p=gsl_complex_mul(lattice[position+mu], lattice[position+nu+neighbour[mu]]); //U_mu(r)*U_nu(r+mu)
-    gsl_complex q=gsl_complex_mul(lattice[position+neighbour[nu]+mu], lattice[position+nu]); //U_mu(r+vu)*U_nu(r)
-    return 1.0-GSL_REAL(gsl_complex_mul(p, gsl_complex_conjugate(q)));
-    //~ return 1.0-cos(lattice[position+mu]+lattice[position+nu+neighbour[mu]]-lattice[position+neighbour[nu]+mu]-lattice[position+nu]);
+double plaquette(double * lattice, int * neighbour, int position, int mu, int nu){
+    //~ gsl_complex p=gsl_complex_mul(lattice[position+mu], lattice[position+nu+neighbour[mu]]); //U_mu(r)*U_nu(r+mu)
+    //~ gsl_complex q=gsl_complex_mul(lattice[position+neighbour[nu]+mu], lattice[position+nu]); //U_mu(r+vu)*U_nu(r)
+    //~ return 1.0-GSL_REAL(gsl_complex_mul(p, gsl_complex_conjugate(q)));
+    return 1.0-cos(lattice[position+mu]+lattice[position+nu+neighbour[mu]]-lattice[position+neighbour[nu]+mu]-lattice[position+nu]);
 }
 
 /**
@@ -31,7 +40,7 @@ double plaquette(gsl_complex * lattice, int * neighbour, int position, int mu, i
  * neighbour implements periodic boundary conditions
  * position keeps the code more legible
  * **/
-double action(gsl_complex *lattice, double beta, double deltatau, int *neighbour, int Nt, int Ns){
+double action(double *lattice, double beta, double deltatau, int *neighbour, int Nt, int Ns){
     int position;
     int spaceaction=0;
     int timeaction=0;
@@ -57,36 +66,66 @@ double action(gsl_complex *lattice, double beta, double deltatau, int *neighbour
     return beta*(deltatau*spaceaction+timeaction/deltatau);
 }
 
+//~ /** 
+ //~ * uses complex numbers in the lattice
+ //~ * calculates the change in action if the link at position, mu is changed
+ //~ * one link is involved in six plaquettes, if the link is changed, the rest does not change. 
+ //~ * uses that only the real part is needed later, so uses Re(ab*)=Re(a*b)
+ //~ * takes into account anisotropy by giving a different weight to temporal contribution
+ //~ * **/
+//~ double deltas(gsl_complex * lattice, int * neighbour, int position, double beta, double deltatau, int mu, gsl_complex change){
+    //~ gsl_complex sum=gsl_complex_rect(0,0);
+    //~ gsl_complex p=gsl_complex_rect(0,0), q=gsl_complex_rect(0,0);
+    //~ for (int nu=0;nu<4;nu+=1){if(nu!=mu){
+        //~ //contribution at position
+        //~ p=gsl_complex_mul(lattice[position+neighbour[nu]+mu], lattice[position+nu]); //U_mu(r+vu)*U_nu(r)
+        //~ p=gsl_complex_mul(lattice[position+nu+neighbour[mu]], gsl_complex_conjugate(p)); //U_nu(r+mu)
+        //~ //contribution at position-nu, use conjugate of plaquette to be able to use unconjugated missing link
+        //~ q=gsl_complex_mul(lattice[position+neighbour[4+nu]+mu], lattice[position+neighbour[4+nu]+neighbour[mu]+nu]); //U_mu(r-vu)*U_nu(r-nu+mu)
+        //~ q=gsl_complex_mul(lattice[position+nu+neighbour[4+nu]], gsl_complex_conjugate(q)); //U_nu(r-nu)
+        //~ p=gsl_complex_add(p,q);
+        //~ if (mu==0||nu==0){
+            //~ p=gsl_complex_mul_real(p, 1.0/deltatau/deltatau);
+        //~ }
+        //~ p=gsl_complex_mul_real(p, beta*deltatau);
+        //~ sum=gsl_complex_add(sum, p);
+    //~ }}
+    //~ //double constantsummand=4*deltatau+2/deltatau;
+    //~ //if(mu==0){constantsummand=6/deltatau;}
+    //~ //beta*constantsummand
+    //~ return -1.0*GSL_REAL(gsl_complex_mul(sum, gsl_complex_sub(lattice[position+mu], gsl_complex_mul(lattice[position+mu], change))));
+//~ }
+
 /** 
- * calculates the change in action if the link at position, mu is changed
- * one link is involved in six plaquettes, if the link is changed, the rest does not change. 
- * uses that only the real part is needed later, so uses Re(ab*)=Re(a*b)
- * takes into account anisotropy by giving a different weight to temporal contribution
+ * calculates change in the action if link variable at position, mu is changed by change
+ * uses only the angle in the lattice
+ * one link contributes to six plaquettes, these plaquettes are all calculated, and the difference between the current and the proposed new state is calculated explicitly
  * **/
-double deltas(gsl_complex * lattice, int * neighbour, int position, double beta, double deltatau, int mu, gsl_complex change){
-    gsl_complex sum=gsl_complex_rect(0,0);
-    gsl_complex p=gsl_complex_rect(0,0), q=gsl_complex_rect(0,0);
+double deltas(double *lattice, int *neighbour, int position, double beta, double deltatau, int mu, double change){
+    double sum=0, oneloop;
+    double value=lattice[position+mu];
+    double propose=lattice[position+mu]+change;
+    //calculate S(value)-S(propose)
     for (int nu=0;nu<4;nu+=1){if(nu!=mu){
-        //contribution at position
-        p=gsl_complex_mul(lattice[position+neighbour[nu]+mu], lattice[position+nu]); //U_mu(r+vu)*U_nu(r)
-        p=gsl_complex_mul(lattice[position+nu+neighbour[mu]], gsl_complex_conjugate(p)); //U_nu(r+mu)
-        //contribution at position-nu, use conjugate of plaquette to be able to use unconjugated missing link
-        q=gsl_complex_mul(lattice[position+neighbour[4+nu]+mu], lattice[position+neighbour[4+nu]+neighbour[mu]+nu]); //U_mu(r-vu)*U_nu(r-nu+mu)
-        q=gsl_complex_mul(lattice[position+nu+neighbour[4+nu]], gsl_complex_conjugate(q)); //U_nu(r-nu)
-        p=gsl_complex_add(p,q);
-        if (mu==0||nu==0){
-            p=gsl_complex_mul_real(p, 1.0/deltatau/deltatau);
+        //at position
+        oneloop=cos(value+lattice[position+nu+neighbour[mu]]-lattice[position+neighbour[nu]+mu]-lattice[position+nu]);
+        oneloop-=cos(propose+lattice[position+nu+neighbour[mu]]-lattice[position+neighbour[nu]+mu]-lattice[position+nu]);
+        //at position-nu
+        oneloop+=cos(lattice[position+neighbour[4+nu]+mu]+lattice[position+neighbour[4+nu]+neighbour[mu]+nu]-value-lattice[position+nu+neighbour[4+nu]]);
+        oneloop-=cos(lattice[position+neighbour[4+nu]+mu]+lattice[position+neighbour[4+nu]+neighbour[mu]+nu]-propose-lattice[position+nu+neighbour[4+nu]]);
+        if (mu==0||nu==0){  //account for anisotropy
+            oneloop/=deltatau*deltatau;
         }
-        p=gsl_complex_mul_real(p, beta*deltatau);
-        sum=gsl_complex_add(sum, p);
+        oneloop*=beta*deltatau;
+        sum+=oneloop;
     }}
-    //~ double constantsummand=4*deltatau+2/deltatau;
-    //~ if(mu==0){constantsummand=6/deltatau;}
-    //~ beta*constantsummand
-    return -1.0*GSL_REAL(gsl_complex_mul(sum, gsl_complex_sub(lattice[position+mu], gsl_complex_mul(lattice[position+mu], change))));
+    return -sum;
 }
 
-void sweep(gsl_complex *lattice, double beta, double deltatau, gsl_rng *generator, FILE * stream, int Nt, int Ns, double delta){
+/** goes through the entire lattice and attempts to change each link once in an ordered sweep
+ * value of the average plaquette is returned, average plaquette and acceptane rate are printed out for plotting
+ * **/
+double sweep(double *lattice, double beta, double deltatau, gsl_rng *generator, FILE * stream, int Nt, int Ns, double delta){
     double change;  
     int neighbour[8], accept=0, averageplaquette=0, latticesites=Ns*Ns*Ns*Nt*4;  
     /**neighbours: need two neighbours for every direction, one forward and one backward
@@ -119,10 +158,9 @@ void sweep(gsl_complex *lattice, double beta, double deltatau, gsl_rng *generato
                     position=Ns*Ns*Ns*4*t+Ns*Ns*4*x+Ns*4*y+4*z;
                     for (int mu=0;mu<4;mu+=1){
                         change=gsl_ran_flat(generator, -delta, delta);
-                        //~ printf("%.1f %.1f\n", deltas(lattice, neighbour, position, beta, deltatau, mu, change), GSL_REAL(change));
-                        //~ printf("%.1f %.1f\n", deltas(lattice, neighbour, position, beta, deltatau, mu, change), GSL_REAL(change));
-                        if(exp(-deltas(lattice, neighbour, position, beta, deltatau, mu, gsl_complex_polar(1, change)))>gsl_rng_uniform(generator)){
-                        lattice[position+mu]=gsl_complex_mul(lattice[position+mu], gsl_complex_polar(1,change));
+                        if(exp(-deltas(lattice, neighbour, position, beta, deltatau, mu, change))>gsl_rng_uniform(generator)){
+                        //~ lattice[position+mu]=gsl_complex_mul(lattice[position+mu], gsl_complex_polar(1,change));
+                        lattice[position+mu]+=change;
                         accept+=1;
                         }
                         for (int nu=0;nu<4;nu+=1){
@@ -134,40 +172,50 @@ void sweep(gsl_complex *lattice, double beta, double deltatau, gsl_rng *generato
         }
     }
     fprintf(stream, "%f\t%f\n", averageplaquette/(4.0*latticesites), accept/(1.0*latticesites));
+    return averageplaquette/(4.0*latticesites);
 }
 
 int main(int argc, char **argv){
-    double beta=1;
-    double deltatau=0.1;   
+    double beta=1.01;
+    double deltatau=1;   
     int Ns=8;       //number of sites for the spatial directions
     int Nt=8;       //number of sites for the temporal directions
     int latticesites=Nt*Ns*Ns*Ns*4;
-    //~ double lattice[latticesites];
-    gsl_complex lattice[latticesites];
+    double measurements=5000;
+    double lattice[latticesites];
+    //~ gsl_complex lattice[latticesites];
     int neighbour[8];
     
     gsl_rng *generator;
 	generator=gsl_rng_alloc(gsl_rng_mt19937);	
     gsl_rng_set(generator, 0);
-    double delta=M_PI*0.2;
+    double delta=M_PI*0.25;     //change delta to change acceptance rate
     
-    FILE * stream=fopen("meas.txt", "w+");
+    //save acceptance rate and average plaquette for plotting
+    char filename[100];
+    sprintf(filename, "measbeta%.1fNt%dNs%d.txt", beta, Nt, Ns);
+    FILE * stream=fopen(filename, "w+");
     
     double averageplaquette=0;
     int accept=0;
-    double calc_action=0;
-    double change;
+    double change;  //stores proposed changes to link
+    gsl_vector * plaq=gsl_vector_alloc(measurements);   //stores measured plaquette values to take average
     
     /** initialize lattice **/
     for (int i=0; i<latticesites; i+=1){
-        lattice[i]=gsl_complex_polar(1,i);
-        //~ lattice[i]=i;
+        //~ lattice[i]=gsl_complex_polar(1,i);
+        lattice[i]=i;
     }
-        printf("blubb\n");
     
     for(int i=0; i<1000;i+=1){
         sweep(lattice, beta, deltatau, generator, stream, Nt, Ns,  delta);
     }
+    printf("thermalization done\n");
+    for(int i=0; i<measurements;i+=1){
+        gsl_vector_set(plaq, i, sweep(lattice, beta, deltatau, generator, stream, Nt, Ns,  delta));
+    }
+    
+    printf("%f\t%f\t%d\t%d\t%f\n", beta, deltatau,Ns, Nt, mean(plaq, measurements));
     
     /**neighbours: need two neighbours for every direction, one forward and one backward
 	 * 0: t-forward     4: t-backward
@@ -199,10 +247,9 @@ int main(int argc, char **argv){
                     position=Ns*Ns*Ns*4*t+Ns*Ns*4*x+Ns*4*y+4*z;
                     for (int mu=0;mu<4;mu+=1){
                         change=gsl_ran_flat(generator, -delta, delta);
-                        //~ printf("%.1f %.1f\n", deltas(lattice, neighbour, position, beta, deltatau, mu, change), GSL_REAL(change));
-                        //~ printf("%.1f %.1f\n", deltas(lattice, neighbour, position, beta, deltatau, mu, change), GSL_REAL(change));
-                        if(exp(-deltas(lattice, neighbour, position, beta, deltatau, mu, gsl_complex_polar(1, change)))>gsl_rng_uniform(generator)){
-                        lattice[position+mu]=gsl_complex_mul(lattice[position+mu], gsl_complex_polar(1,change));
+                        if(exp(-deltas(lattice, neighbour, position, beta, deltatau, mu, change))>gsl_rng_uniform(generator)){
+                        //~ lattice[position+mu]=gsl_complex_mul(lattice[position+mu], gsl_complex_polar(1,change));
+                        lattice[position+mu]+=change;
                         accept+=1;
                         }
                         //~ for (int nu=0;nu<4;nu+=1){
@@ -217,6 +264,7 @@ int main(int argc, char **argv){
     printf("%f\n", accept/(1.0*latticesites)); 
     gsl_rng_free(generator);
     fclose(stream);
+    gsl_vector_free(plaq);
 	return 0;
 }
 
